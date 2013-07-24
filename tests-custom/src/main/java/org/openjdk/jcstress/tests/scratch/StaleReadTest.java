655037e4e1a1620b37fe98344436e8208ba70544
Aleksey Shipilev <shade@openjdk.org>
2013-07-24 13:24:46 +0400

    Tests-custom: Sample stale read test

---
+++ tests-custom/src/main/java/org/openjdk/jcstress/tests/scratch/StaleReadTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jcstress.tests.scratch;
+
+import org.openjdk.jcstress.tests.TerminationTest;
+
+public class StaleReadTest implements TerminationTest<StaleReadTest.State> {
+
+    @Override
+    public void actor1(State state) throws Exception {
+        int la = 0;
+        int lg = 0;
+        while (la == 0) {
+            lg = state.g; // "force re-read"
+            la = state.a;
+        }
+    }
+
+    @Override
+    public void signal(State state, Thread actor1) throws Exception {
+        state.a = 1;
+    }
+
+    @Override
+    public State newState() {
+        return new State();
+    }
+
+    public static class State {
+        volatile int g;
+        int a;
+    }
+
+}
