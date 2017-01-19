615309b140a5111e1ed31b4e51e584a4331a39c2
Aleksey Shipilev <shade@openjdk.org>
2017-01-19 22:32:50 +0100

    Jcstress-samples

---
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/tests/JCStressSample_01_IncrementAtomicity.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2016, Red Hat Inc.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jcstress.tests;
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.IntResult2;
+
+/*
+    This is our first concurrency test. It is deliberately simplistic to show
+    testing approaches, introduce JCStress APIs, etc.
+
+    Suppose we want to see if the field increment is atomic. We can make test
+    with two actors, both actors incrementing the field and recording what
+    value they observed into the result object. As JCStress runs, it will
+    invoke these methods on the objects holding the field once per each actor
+    and instance, and record what results are coming from there.
+
+    Done enough times, we will get the history of observed results, and that
+    would tell us something about the concurrent behavior. For example, running
+    this test would yield:
+
+          [OK] o.o.j.t.JCStressSample_01_IncrementAtomicity
+        (JVM args: [-server])
+      Observed state   Occurrences   Expectation  Interpretation
+                1, 1    54,734,140    ACCEPTABLE  Both threads came up with the same value: atomicity failure.
+                1, 2    47,037,891    ACCEPTABLE  actor1 incremented, then actor2.
+                2, 1    53,204,629    ACCEPTABLE  actor2 incremented, then actor1.
+ */
+
+@JCStressTest
+
+// These are the test outcomes.
+@Outcome(id = "1, 1", expect = Expect.ACCEPTABLE, desc = "Both actors came up with the same value: atomicity failure.")
+@Outcome(id = "1, 2", expect = Expect.ACCEPTABLE, desc = "actor1 incremented, then actor2.")
+@Outcome(id = "2, 1", expect = Expect.ACCEPTABLE, desc = "actor2 incremented, then actor1.")
+
+// This is a state object
+@State
+public class JCStressSample_01_IncrementAtomicity {
+
+    int v;
+
+    @Actor
+    public void actor1(IntResult2 r) {
+        r.r1 = ++v; // record result from actor1 to field r1
+    }
+
+    @Actor
+    public void actor2(IntResult2 r) {
+        r.r2 = ++v; // record result from actor2 to field r2
+    }
+
+}
