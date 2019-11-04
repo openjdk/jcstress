6b5ebc3f93d5284cabe5b25e936b67c6376f3574
Aleksey Shipilev <shade@openjdk.org>
2019-11-04 13:16:18 +0100

    New SynchronizedMutexPoolTest

---
+++ tests-custom/src/main/java/org/openjdk/jcstress/tests/locks/mutex/SynchronizedMutexPoolTest.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jcstress.tests.locks.mutex;
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.II_Result;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+@JCStressTest
+@Outcome(id = "1, 0", expect = Expect.ACCEPTABLE, desc = "T1 -> T2 execution.")
+@Outcome(id = "0, 2", expect = Expect.ACCEPTABLE, desc = "T2 -> T1 execution.")
+@State
+public class SynchronizedMutexPoolTest {
+
+    static final Object[] LOCKS = new Object[1000];
+    static {
+        Arrays.fill(LOCKS, new byte[1_000]);
+    }
+
+    static Object select() {
+        return LOCKS[ThreadLocalRandom.current().nextInt(LOCKS.length)];
+    }
+
+    final Object lock = select();
+    int value;
+
+    @Actor
+    public void actor1(II_Result r) {
+        synchronized (lock) {
+            r.r1 = (value == 0) ? 1 : 0;
+            value = 1;
+        }
+    }
+
+    @Actor
+    public void actor2(II_Result r) {
+        synchronized (lock) {
+            r.r2 = (value == 0) ? 2 : 0;
+            value = 1;
+        }
+    }
+
+}
