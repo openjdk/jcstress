fe5bebde31985d1617aa4e7ce33b67772de613b9
Volker Simonis <simonis@openjdk.org>
2020-12-16 11:31:06 +0000

    7902812: Fields can still be default initialized if class has only static intializers
    Reviewed-by: shade

--- jcstress-core/src/main/java/org/openjdk/jcstress/infra/processors/JCStressTestProcessor.java
+++ jcstress-core/src/main/java/org/openjdk/jcstress/infra/processors/JCStressTestProcessor.java
@@ -676,8 +676,9 @@ public class JCStressTestProcessor extends AbstractProcessor {
                 if (t.getInitializer() != null) return false;
             }
             if (member.getKind() == Tree.Kind.BLOCK) {
+                BlockTree b = (BlockTree) member;
                 // no instance initializers of any kind
-                return false;
+                if (!b.isStatic()) return false;
             }
         }
         return true;
