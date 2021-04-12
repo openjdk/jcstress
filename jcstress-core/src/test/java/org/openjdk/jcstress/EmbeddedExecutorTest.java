86b46cc8b4eb36e3e2164699ab306f3f620fa9ff
Aleksey Shipilëv <shade@redhat.com>
2021-04-12 19:48:09 +0200

    CODETOOLS-7902881: Upgrade to JUnit 4.13.2

--- jcstress-core/src/test/java/org/openjdk/jcstress/EmbeddedExecutorTest.java
+++ jcstress-core/src/test/java/org/openjdk/jcstress/EmbeddedExecutorTest.java
@@ -25,7 +25,7 @@
 
 package org.openjdk.jcstress;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openjdk.jcstress.infra.Status;
 import org.openjdk.jcstress.infra.TestInfo;
