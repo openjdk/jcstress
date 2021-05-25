5af158337cf71fb7cc4512151cdcc041a7db5776
Aleksey Shipilev <shade@redhat.com>
2021-05-25 17:45:49 +0200

    Remove excess method

--- jcstress-core/src/main/java/org/openjdk/jcstress/util/StringUtils.java
+++ jcstress-core/src/main/java/org/openjdk/jcstress/util/StringUtils.java
@@ -168,14 +168,6 @@ public class StringUtils {
         return r;
     }
 
-    public static String repeat(String s, int count) {
-        StringBuilder sb = new StringBuilder();
-        for (int c = 0; c < count; c++) {
-            sb.append(s);
-        }
-        return sb.toString();
-    }
-
     public static String percent(long v, long total, int prec) {
         double p = v * 100.0 / total;
         double limit = Math.pow(0.1, prec);
