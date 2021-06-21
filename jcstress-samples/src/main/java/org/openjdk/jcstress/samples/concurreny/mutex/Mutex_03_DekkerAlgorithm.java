4f0e20997740aa7436bcc441d4a88ba8d5fe05d2
mmirwaldt <michael.mirwaldt@gmail.com>
2021-06-21 17:18:08 +0200

    added 3 samples for mutex algorithms

---
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/samples/concurreny/mutex/Mutex_03_DekkerAlgorithm.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jcstress.samples.concurreny.mutex;
+
+import org.openjdk.jcstress.annotations.Actor;
+import org.openjdk.jcstress.annotations.JCStressTest;
+import org.openjdk.jcstress.annotations.Outcome;
+import org.openjdk.jcstress.annotations.State;
+import org.openjdk.jcstress.infra.results.II_Result;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
+import static org.openjdk.jcstress.annotations.Expect.FORBIDDEN;
+
+public class Mutex_03_DekkerAlgorithm {
+
+    /*
+        How to run this test:
+            $ java -jar jcstress-samples/target/jcstress.jar -t Mutex_03_DekkerAlgorithm
+     */
+
+    /**
+     * Implemented according to https://en.wikipedia.org/wiki/Dekker%27s_algorithm
+     */
+    @JCStressTest
+    @Outcome(id = {"1, 1"}, expect = ACCEPTABLE, desc = "Both actors could enter the critical section")
+    @Outcome(id = {"1, 2", "2, 1", "2, 2"}, expect = FORBIDDEN, desc = "At least one actor couldn't enter the critical section")
+    @Outcome(id = {"0, 0", "0, 1", "1, 0", "0, 2", "2, 0"}, expect = FORBIDDEN, desc = "At least one actor hang up in one of the loops")
+    @State
+    public static class DekkerAlgorithm {
+        private final AtomicBoolean actor1wantsToEnter = new AtomicBoolean();
+        private final AtomicBoolean actor2wantsToEnter = new AtomicBoolean();
+        private final AtomicInteger turn = new AtomicInteger(0);
+        private final AtomicBoolean isInCriticalSection = new AtomicBoolean();
+
+        @Actor
+        public void actor1(II_Result r) {
+            actor1wantsToEnter.set(true);
+            while (actor2wantsToEnter.get()) {
+                if (turn.get() != 0) {
+                    actor1wantsToEnter.set(false);
+                    while (turn.get() != 0) ;
+                    actor1wantsToEnter.set(true);
+                }
+            }
+            if (isInCriticalSection.compareAndSet(false, true)) {
+                r.r1 = 1;
+                isInCriticalSection.set(false);
+            } else {
+                r.r1 = 2;
+            }
+            turn.set(1);
+            actor1wantsToEnter.set(false);
+        }
+
+        @Actor
+        public void actor2(II_Result r) {
+            actor2wantsToEnter.set(true);
+            while (actor1wantsToEnter.get()) {
+                if (turn.get() != 1) {
+                    actor2wantsToEnter.set(false);
+                    while (turn.get() != 1) ;
+                    actor2wantsToEnter.set(true);
+                }
+            }
+            if (isInCriticalSection.compareAndSet(false, true)) {
+                r.r2 = 1;
+                isInCriticalSection.set(false);
+            } else {
+                r.r2 = 2;
+            }
+            turn.set(0);
+            actor2wantsToEnter.set(false);
+        }
+    }
+}
