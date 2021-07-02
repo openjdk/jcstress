72c7635d20332081970b85598214e32fbc048a22
Michael Mirwaldt <michael.mirwaldt@gmail.com>
2021-07-02 22:37:50 +0200

    Changed user name and email

---
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/samples/concurrency/mutex/Mutex_01_PetersonAlgorithm.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jcstress.samples.concurrency.mutex;
+
+import org.openjdk.jcstress.annotations.Actor;
+import org.openjdk.jcstress.annotations.JCStressTest;
+import org.openjdk.jcstress.annotations.Outcome;
+import org.openjdk.jcstress.annotations.State;
+import org.openjdk.jcstress.infra.results.II_Result;
+
+import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
+import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE_INTERESTING;
+
+/*
+    How to run this test:
+        $ java -jar jcstress-samples/target/jcstress.jar -t Mutex_01_PetersonAlgorithm
+ */
+
+/**
+ * Implemented according to https://en.wikipedia.org/wiki/Peterson%27s_algorithm
+ */
+@JCStressTest
+@Outcome(id = {"1, 2", "2, 1"}, expect = ACCEPTABLE, desc = "Sequential execution.")
+@Outcome(id = "1, 1", expect = ACCEPTABLE_INTERESTING, desc = "Both actors came up with the same value: lock failure.")
+@State
+public class Mutex_01_PetersonAlgorithm {
+    private volatile boolean flagForActor1;
+    private volatile boolean flagForActor2;
+    private volatile int turn;
+    private int v;
+
+    @Actor
+    public void actor1(II_Result r) {
+        flagForActor1 = true;
+        turn = 2;
+        while (flagForActor2 && turn == 2); // wait
+        { // critical section
+            r.r1 = ++v;
+        }
+        flagForActor1 = false;
+    }
+
+    @Actor
+    public void actor2(II_Result r) {
+        flagForActor2 = true;
+        turn = 1;
+        while (flagForActor1 && turn == 1); // wait
+        { // critical section
+            r.r2 = ++v;
+        }
+        flagForActor2 = false;
+    }
+}
