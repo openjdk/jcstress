bc8fbadb92bb4248df303c156f2f8c96ed839c47
mmirwaldt <michael.mirwaldt@gmail.com>
2021-09-28 20:01:29 +0200

    reduced the ClassicProblem_01_DiningPhilosophers to the resource hierarchy solution

--- jcstress-samples/src/main/java/org/openjdk/jcstress/samples/concurrency/classic/ClassicProblem_01_DiningPhilosophers.java
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/samples/concurrency/classic/ClassicProblem_01_DiningPhilosophers.java
@@ -37,206 +37,55 @@ import static org.openjdk.jcstress.annotations.Expect.FORBIDDEN;
 /*
     How to run this test:
         $ java -jar jcstress-samples/target/jcstress.jar -t ClassicProblem_01_DiningPhilosophers
-
-    If you run this sample with Java 15+, check the -XX:+UseBiasedLocking flag for monitors
-    because biased locking is disabled by default which can harm the performance.
  */
 
 /**
  * This sample shows you how JCStress can help you to test solutions for the famous Dining philosophers problem.
- * It solves this problem by monitors, reentrant locks and semaphores.
+ * It solves this problem by a resource hierarchy.
  * See https://en.wikipedia.org/wiki/Dining_philosophers_problem for more information about the problem.
  */
-public abstract class ClassicProblem_01_DiningPhilosophers {
+public class ClassicProblem_01_DiningPhilosophers {
+    @JCStressTest
+    @Outcome(id = {"true"}, expect = ACCEPTABLE, desc = "All philosophers could eat with their 2 neighboured forks.")
+    @Outcome(expect = FORBIDDEN, desc = "At least one philosopher couldn't eat.")
+    @State
+    public static class ResourceHierarchy {
+        private final Semaphore[] semaphores =
+                new Semaphore[]{new Semaphore(1), new Semaphore(1), new Semaphore(1)};
 
-    static abstract class Base {
+        @Actor
+        public void p1() {
+            // think
+            eat(0, 1);
+        }
+
+        @Actor
+        public void p2() {
+            // think
+            eat(1, 2);
+        }
+
+        @Actor
+        public void p3() {
+            // think
+            eat(0, 2); // and not eat(2, 0) because we must acquire all locks in the same order to avoid deadlocks
+        }
+
+        @Arbiter
+        public void fake(Z_Result r) {
+            r.r1 = true;
+        }
+
         final protected void eat(int fork1, int fork2) {
             try {
-                pickFork(fork1);
-                pickFork(fork2);
+                semaphores[fork1].acquire();
+                semaphores[fork2].acquire();
                 // eating
-                dropFork(fork1);
-                dropFork(fork2);
+                semaphores[fork1].release();
+                semaphores[fork2].release();
             } catch (InterruptedException e) {
                 throw new IllegalStateException(e);
             }
-        }
-
-        protected abstract void pickFork(int fork) throws InterruptedException;
-
-        protected abstract void dropFork(int fork);
-    }
-
-    @JCStressTest
-    @Outcome(id = {"true"}, expect = ACCEPTABLE, desc = "All philosophers could eat with their 2 neighboured forks.")
-    @Outcome(expect = FORBIDDEN, desc = "At least one philosopher couldn't eat.")
-    @State
-    public static class Monitors extends Base {
-        private final Object lock = new Object();
-        private final boolean[] forksInUse = new boolean[4];
-
-        @Actor
-        public void p1() {
-            // think
-            eat(0, 1);
-        }
-
-        @Actor
-        public void p2() {
-            // think
-            eat(1, 2);
-        }
-
-        @Actor
-        public void p3() {
-            // think
-            eat(2, 3);
-        }
-
-        @Actor
-        public void p4() {
-            // think
-            eat(0, 3); // and not eat(3, 0) because we must acquire all locks in the same order to avoid deadlocks
-        }
-
-        @Arbiter
-        public void fake(Z_Result r) {
-            r.r1 = true;
-        }
-
-        @Override
-        protected void pickFork(int fork) {
-            synchronized (lock) {
-                while (forksInUse[fork]) {
-                    try {
-                        lock.wait();
-                    } catch (InterruptedException e) {
-                        throw new IllegalStateException(e);
-                    }
-                    forksInUse[fork] = true;
-                }
-            }
-        }
-
-        @Override
-        protected void dropFork(int fork) {
-            synchronized (lock) {
-                forksInUse[fork] = false;
-                lock.notifyAll();
-            }
-        }
-    }
-
-
-    @JCStressTest
-    @Outcome(id = {"true"}, expect = ACCEPTABLE, desc = "All philosophers could eat with their 2 neighboured forks.")
-    @Outcome(expect = FORBIDDEN, desc = "At least one philosopher couldn't eat.")
-    @State
-    public static class ReentrantLocks extends Base {
-        private final ReentrantLock lock = new ReentrantLock();
-        private final Condition forkReleased = lock.newCondition();
-        private final boolean[] forksInUse = new boolean[4];
-
-        @Actor
-        public void p1() {
-            // think
-            eat(0, 1);
-        }
-
-        @Actor
-        public void p2() {
-            // think
-            eat(1, 2);
-        }
-
-        @Actor
-        public void p3() {
-            // think
-            eat(2, 3);
-        }
-
-        @Actor
-        public void p4() {
-            // think
-            eat(0, 3); // and not eat(3, 0) because we must acquire all locks in the same order to avoid deadlocks
-        }
-
-        @Arbiter
-        public void fake(Z_Result r) {
-            r.r1 = true;
-        }
-
-        @Override
-        protected void pickFork(int fork) throws InterruptedException {
-            lock.lock();
-            try {
-                while(forksInUse[fork]) {
-                    forkReleased.await();
-                }
-                forksInUse[fork] = true;
-            } finally {
-                lock.unlock();
-            }
-        }
-
-        @Override
-        protected void dropFork(int fork) {
-            lock.lock();
-            try {
-                forksInUse[fork] = false;
-                forkReleased.signalAll();
-            } finally {
-                lock.unlock();
-            }
-        }
-    }
-
-    @JCStressTest
-    @Outcome(id = {"true"}, expect = ACCEPTABLE, desc = "All philosophers could eat with their 2 neighboured forks.")
-    @Outcome(expect = FORBIDDEN, desc = "At least one philosopher couldn't eat.")
-    @State
-    public static class Semaphores extends Base {
-        private final Semaphore[] semaphores =
-                new Semaphore[] {
-                        new Semaphore(1), new Semaphore(1),
-                        new Semaphore(1), new Semaphore(1)};
-        @Actor
-        public void p1() {
-            // think
-            eat(0, 1);
-        }
-
-        @Actor
-        public void p2() {
-            // think
-            eat(1, 2);
-        }
-
-        @Actor
-        public void p3() {
-            // think
-            eat(2, 3);
-        }
-
-        @Actor
-        public void p4() {
-            // think
-            eat(0, 3); // and not eat(3, 0) because we must acquire all locks in the same order to avoid deadlocks
-        }
-
-        @Arbiter
-        public void fake(Z_Result r) {
-            r.r1 = true;
-        }
-
-        @Override
-        protected void pickFork(int fork) throws InterruptedException {
-            semaphores[fork].acquire();
-        }
-
-        @Override
-        protected void dropFork(int fork) {
-            semaphores[fork].release();
         }
     }
 }
