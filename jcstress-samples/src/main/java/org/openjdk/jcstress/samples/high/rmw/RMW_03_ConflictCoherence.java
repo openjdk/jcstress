7262c60ba09248c5a9a01f229684992eee40cc66
Aleksey Shipilev <shade@redhat.com>
2021-11-01 16:17:00 +0100

    Updates

--- jcstress-samples/src/main/java/org/openjdk/jcstress/samples/high/rmw/RMW_03_ConflictCoherence.java
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/samples/high/rmw/RMW_03_ConflictCoherence.java
@@ -40,17 +40,35 @@ import static org.openjdk.jcstress.annotations.Expect.*;
 @Outcome(id = "false, false",                 expect = ACCEPTABLE, desc = "Not even once")
 @Outcome(id = "true, true",                   expect = FORBIDDEN,  desc = "More than once")
 @State
-public class RMW_03_Contended_Coherence {
+public class RMW_03_ConflictCoherence {
 
     /*
         How to run this test:
-            $ java -jar jcstress-samples/target/jcstress.jar -t RMW_03_Contended_Coherence[.SubTestName]
+            $ java -jar jcstress-samples/target/jcstress.jar -t RMW_03_ConflictCoherence[.SubTestName]
      */
 
     /*
       ----------------------------------------------------------------------------------------------------------
 
-        ....
+        This example shows that even with the weakest form of RMW (CAS), without any memory ordering
+        whatsoever, we are still covered by coherence (see BasicJMM_05_Coherence example). That is,
+        a conflicting RMW operation cannot see the "stale" value and succeed to update the location
+        the second time.
+
+        In this example, even though the store of "1" happens concurrently with both CASes,
+        only one CAS is allowed to succeed. This test specifically uses the weakest non-barrier
+        form of CAS to point that it is the property of the accesses themselves, not their
+        implied memory ordering.
+
+        Indeed, on all platforms (x86_64, AArch64, PPC64), this would happen:
+                RESULT        SAMPLES     FREQ      EXPECT  DESCRIPTION
+          false, false    672,734,373   15.43%  Acceptable  Not even once
+           false, true  1,757,321,910   40.31%  Acceptable  Trivial
+           true, false  1,929,492,645   44.26%  Acceptable  Trivial
+            true, true              0    0.00%   Forbidden  More than once
+
+        Both CASes can fail (they are weak, and the store to "1" might not happen yet), but
+        they never succeed at the same time.
      */
 
     private int v;
@@ -58,7 +76,7 @@ public class RMW_03_Contended_Coherence {
 
     static {
         try {
-            VH = MethodHandles.lookup().findVarHandle(RMW_03_Contended_Coherence.class, "v", int.class);
+            VH = MethodHandles.lookup().findVarHandle(RMW_03_ConflictCoherence.class, "v", int.class);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             throw new IllegalStateException(e);
         }
