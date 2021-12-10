14a1753543ed41762e0452e2429f1a590fb6e45a
mmirwaldt <michael.mirwaldt@gmail.com>
2021-12-10 15:08:06 +0100

    applied proposals from the fifth review on sample Classic_02_ProducerConsumerProblem

--- jcstress-samples/src/main/java/org/openjdk/jcstress/samples/problems/classic/Classic_02_ProducerConsumerProblem.java
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/samples/problems/classic/Classic_02_ProducerConsumerProblem.java
@@ -59,7 +59,7 @@ public class Classic_02_ProducerConsumerProblem {
     /*
       ----------------------------------------------------------------------------------------------------------
 
-        One solution uses semaphores to solve the producer-consumer problem.
+        One general solution uses semaphores to solve the producer-consumer problem.
      */
     static class SemaphoresBase {
         protected final Semaphore fillCount = new Semaphore(BUFFER_SIZE);
@@ -112,6 +112,9 @@ public class Classic_02_ProducerConsumerProblem {
 
         This solution shows how semaphores can be used to solve the producer-consumer problem
         for only one producer and only one consumer.
+
+          RESULT      SAMPLES     FREQ      EXPECT  DESCRIPTION
+            true  685.944.832  100,00%  Acceptable  Trivial
      */
     @JCStressTest
     @Outcome(id = "true", expect = ACCEPTABLE, desc = "Trivial")
@@ -143,16 +146,17 @@ public class Classic_02_ProducerConsumerProblem {
         Both producers might use the same index at the same time
         to put their elements into the buffer so that they overwrite each other's item.
 
-          RESULT     SAMPLES     FREQ       EXPECT  DESCRIPTION
-          0, 0, 0      61.158    0,37%   Acceptable  Producers didn't overwrite each other's item.
-          0, 0, 1  15.066.987   91,84%   Acceptable  Producers didn't overwrite each other's item.
-          0, 0, 2       9.078    0,06%  Interesting  Producers overwrote each other's item.
-          0, 1, 0      23.658    0,14%   Acceptable  Producers didn't overwrite each other's item.
-          0, 1, 1      80.552    0,49%   Acceptable  Producers didn't overwrite each other's item.
-          0, 1, 2     799.311    4,87%   Acceptable  Producers didn't overwrite each other's item.
-          1, 0, 0      48.668    0,30%   Acceptable  Producers didn't overwrite each other's item.
-          1, 0, 1      66.056    0,40%   Acceptable  Producers didn't overwrite each other's item.
-          1, 0, 2     251.060    1,53%   Acceptable  Producers didn't overwrite each other's item.
+        Indeed, producers overwrote each other's item sometimes:
+           RESULT        SAMPLES     FREQ       EXPECT  DESCRIPTION
+          0, 0, 0     59.200.866    2,92%   Acceptable  Producers didn't overwrite each other's item.
+          0, 0, 1  1.040.776.942   51,41%   Acceptable  Producers didn't overwrite each other's item.
+          0, 0, 2     16.835.996    0,83%  Interesting  Producers overwrote each other's item.
+          0, 1, 0     11.977.104    0,59%   Acceptable  Producers didn't overwrite each other's item.
+          0, 1, 1     41.980.639    2,07%   Acceptable  Producers didn't overwrite each other's item.
+          0, 1, 2    401.250.723   19,82%   Acceptable  Producers didn't overwrite each other's item.
+          1, 0, 0     11.891.016    0,59%   Acceptable  Producers didn't overwrite each other's item.
+          1, 0, 1     41.786.655    2,06%   Acceptable  Producers didn't overwrite each other's item.
+          1, 0, 2    398.936.475   19,70%   Acceptable  Producers didn't overwrite each other's item.
      */
     @JCStressTest
     @Outcome(id = "0, 0, 2", expect = ACCEPTABLE_INTERESTING, desc = "Producers overwrote each other's item.")
@@ -181,6 +185,18 @@ public class Classic_02_ProducerConsumerProblem {
         The solution with semaphores can be extended so that more than one producer and consumer are supported.
         It uses a separate lock to synchronize the access to index counter.
         This makes it impossible for producers to accidentally use the same index.
+
+        Indeed, no producers overwrote each other's item anymore:
+           RESULT      SAMPLES     FREQ       EXPECT  DESCRIPTION
+          0, 0, 0   27.446.203    2,37%   Acceptable  Producers didn't overwrite each other's item.
+          0, 0, 1  434.333.101   37,54%   Acceptable  Producers didn't overwrite each other's item.
+          0, 0, 2            0    0,00%  Interesting  Producers overwrote each other's item.
+          0, 1, 0   12.767.116    1,10%   Acceptable  Producers didn't overwrite each other's item.
+          0, 1, 1   34.391.003    2,97%   Acceptable  Producers didn't overwrite each other's item.
+          0, 1, 2  303.313.651   26,22%   Acceptable  Producers didn't overwrite each other's item.
+          1, 0, 0   12.692.355    1,10%   Acceptable  Producers didn't overwrite each other's item.
+          1, 0, 1   34.471.221    2,98%   Acceptable  Producers didn't overwrite each other's item.
+          1, 0, 2  297.432.966   25,71%   Acceptable  Producers didn't overwrite each other's item.
      */
     @JCStressTest
     @Outcome(id = "0, 0, 2", expect = ACCEPTABLE_INTERESTING, desc = "Producers overwrote each other's item.")
@@ -224,7 +240,14 @@ public class Classic_02_ProducerConsumerProblem {
       ----------------------------------------------------------------------------------------------------------
 
         This solution with a ReentrantLock and two conditions works with many producers and many consumers.
-        While the condition full wakes up producers when
+        While the condition "full" wakes up producers when free space for new items is available,
+        the condition "empty" wakes up consumers when new items can be consumed.
+
+           RESULT      SAMPLES     FREQ       EXPECT  DESCRIPTION
+          0, 0, 1  394.821.232   37,97%   Acceptable  Producers didn't overwrite each other's item.
+          0, 0, 2            0    0,00%  Interesting  Producers overwrote each other's item.
+          0, 1, 2  331.898.566   31,92%   Acceptable  Producers didn't overwrite each other's item.
+          1, 0, 2  313.187.018   30,12%   Acceptable  Producers didn't overwrite each other's item.
      */
     @JCStressTest
     @Outcome(id = "0, 0, 2", expect = ACCEPTABLE_INTERESTING, desc = "Producers overwrote each other's item.")
@@ -295,6 +318,9 @@ public class Classic_02_ProducerConsumerProblem {
 
         This solution with AtomicIntegers only works with one producer and one consumer.
         It fails if an int overflow happens.
+
+          RESULT      SAMPLES     FREQ      EXPECT  DESCRIPTION
+            true  558.057.472  100,00%  Acceptable  One producer produced 2 items which were consumed.
      */
     @JCStressTest
     @Outcome(id = "true", expect = ACCEPTABLE, desc = "One producer produced 2 items which were consumed.")
