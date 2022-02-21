6aa67ea7294647dcc5d3707730f1a93f128ae6ab
Aleksey Shipilev <shade@redhat.com>
2022-02-21 16:15:03 +0300

    Work

--- jcstress-samples/src/main/java/org/openjdk/jcstress/samples/jmm/advanced/AdvancedJMM_06_SemiSynchronized.java
+++ jcstress-samples/src/main/java/org/openjdk/jcstress/samples/jmm/advanced/AdvancedJMM_06_SemiSynchronized.java
@@ -40,59 +40,53 @@ public class AdvancedJMM_06_SemiSynchronized {
      */
 
     static class Composite {
-        int x;
+        int x1, x2, x3, x4;
         public Composite(int v) {
-            x = v;
+            x1 = v; x2 = v; x3 = v; x4 = v;
         }
         public int get() {
-            return x;
+            return x4 + x3 + x2 + x1;
         }
     }
+
 
     /*
       ----------------------------------------------------------------------------------------------------------
 
-        Somewhat similar to previous example, this test now publishes the Composite with the synchronized
-        setter. But, the getter is deliberately non-synchronized. Unfortunately, synchronizing only the
-        setter is not enough: the getter is still racy, and can observe surprising results.
+        Somewhat similar to previous example, this test now exchanges the Composite directly between the
+        threads. In this example, we synchronize the reader. But the writer is deliberately non-synchronized.
+        Unfortunately, synchronizing only the reader part is not enough: the writer is still racy, and can
+        perform the writes in whatever order, thus revealing the surprising results to the casual observer.
 
-        This can be seen on some platforms, for example with PPC64 (modern JDKs require -XX:+UseBiasedLocking):
-          RESULT      SAMPLES     FREQ       EXPECT  DESCRIPTION
-              -1  566,235,858   91.44%   Acceptable  Boring
-               0          354   <0.01%  Interesting  Whoa
-              42   53,010,764    8.56%   Acceptable  Boring
+        This can be seen on some platforms, for example AArch64, PPC64, and it becomes even more visible
+        with -XX:+UseBiasedLocking:
+
+          RESULT        SAMPLES     FREQ       EXPECT  DESCRIPTION
+              -1  2,777,131,721   83.31%   Acceptable  Boring
+               0          1,536   <0.01%  Interesting  Whoa
+               1             52   <0.01%  Interesting  Whoa
+               2             23   <0.01%  Interesting  Whoa
+               3             67   <0.01%  Interesting  Whoa
+               4    556,205,737   16.69%   Acceptable  Boring
      */
 
     @JCStressTest
     @State
-    @Outcome(id = {"-1", "42"}, expect = ACCEPTABLE,             desc = "Boring")
-    @Outcome(id = "0",          expect = ACCEPTABLE_INTERESTING, desc = "Whoa")
-    public static class Racy {
-        Holder<Composite> h = new Holder<>(new Composite(-1));
+    @Outcome(id = {"-1", "4"}, expect = ACCEPTABLE,             desc = "Boring")
+    @Outcome(                  expect = ACCEPTABLE_INTERESTING, desc = "Whoa")
+    public static class RacyWrite {
+        Composite c;
 
         @Actor
-        void actor() {
-            h.set(new Composite(42));
+        void writer() {
+            c = new Composite(1);
         }
 
         @Actor
-        void observer(I_Result r) {
-            r.r1 = h.get().get();
-        }
-
-        static class Holder<T> {
-            T value;
-
-            public Holder(T v) {
-                value = v;
-            }
-
-            public synchronized void set(T v) {
-                value = v;
-            }
-
-            public T get() { // Deliberately not synchronized
-                return value;
+        void reader(I_Result r) {
+            synchronized (this) {
+                Composite lc = c;
+                r.r1 = (lc != null) ? lc.get() : -1;
             }
         }
     }
@@ -100,44 +94,71 @@ public class AdvancedJMM_06_SemiSynchronized {
     /*
       ----------------------------------------------------------------------------------------------------------
 
-        If we properly synchronize both getter and setter, the previously interesting example is now forbidden.
+        Nothing really changes if we synchronize writer instead of reader. What happens within the synchronized
+        block can be the same as in the example above, and reader would observe it, regardless the synchronization
+        block, since it does not synchronize with the writer.
 
-        PPC64:
+        Again, on AArch64 and PPC64:
+          RESULT        SAMPLES     FREQ       EXPECT  DESCRIPTION
+              -1  2,986,368,606   89.69%   Acceptable  Boring
+               0          2,118   <0.01%  Interesting  Whoa
+               1            121   <0.01%  Interesting  Whoa
+               2             51   <0.01%  Interesting  Whoa
+               3            161   <0.01%  Interesting  Whoa
+               4    343,332,879   10.31%   Acceptable  Boring
+     */
+
+    @JCStressTest
+    @State
+    @Outcome(id = {"-1", "4"}, expect = ACCEPTABLE,             desc = "Boring")
+    @Outcome(                  expect = ACCEPTABLE_INTERESTING, desc = "Whoa")
+    public static class RacyRead {
+        Composite c;
+
+        @Actor
+        void writer() {
+            synchronized (this) {
+                c = new Composite(1);
+            }
+        }
+
+        @Actor
+        void reader(I_Result r) {
+            Composite lc = c;
+            r.r1 = (lc != null) ? lc.get() : -1;
+        }
+    }
+
+    /*
+      ----------------------------------------------------------------------------------------------------------
+
+        If we properly synchronize both reader and writer parts, the previously interesting
+        outcomes are now forbidden.
+
+        AArch64, PPC64:
           RESULT      SAMPLES     FREQ      EXPECT  DESCRIPTION
-              -1  229,147,333   43.33%  Acceptable  Boring
-               0            0    0.00%   Forbidden  Now forbidden
-              42  299,711,163   56.67%  Acceptable  Boring
+              -1  927,516,147   50.71%  Acceptable  Boring
+               4  901,720,589   49.29%  Acceptable  Boring
      */
     @JCStressTest
     @State
-    @Outcome(id = {"-1", "42"}, expect = ACCEPTABLE, desc = "Boring")
-    @Outcome(id = "0",          expect = FORBIDDEN,  desc = "Now forbidden")
+    @Outcome(id = {"-1", "4"}, expect = ACCEPTABLE, desc = "Boring")
+    @Outcome(                  expect = FORBIDDEN,  desc = "Now forbidden")
     public static class NonRacy {
-        Holder<Composite> h = new Holder<>(new Composite(-1));
+        Composite c;
 
         @Actor
         void actor() {
-            h.set(new Composite(42));
+            synchronized (this) {
+                c = new Composite(1);
+            }
         }
 
         @Actor
         void observer(I_Result r) {
-            r.r1 = h.get().get();
-        }
-
-        static class Holder<T> {
-            T value;
-
-            public Holder(T v) {
-                value = v;
-            }
-
-            public synchronized void set(T v) {
-                value = v;
-            }
-
-            public synchronized T get() {
-                return value;
+            synchronized (this) {
+                Composite lc = c;
+                r.r1 = (lc != null) ? lc.get() : -1;
             }
         }
     }
