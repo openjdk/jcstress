6284b71a8c40b27f3f8d7c2dbbea037597126215
Aleksey Shipilëv <shade@redhat.com>
2023-01-16 12:41:13 +0100

    7903408: jcstress: Add BiasedLocking options in test configurations

--- jcstress-core/src/main/java/org/openjdk/jcstress/vm/VMSupport.java
+++ jcstress-core/src/main/java/org/openjdk/jcstress/vm/VMSupport.java
@@ -55,6 +55,8 @@ public class VMSupport {
     private static volatile boolean C2_AVAILABLE;
     private static volatile boolean COMPILERS_AVAILABLE;
 
+    private static volatile boolean BIASED_LOCKING_AVAILABLE;
+
     public static boolean spinWaitHintAvailable() {
         return THREAD_SPIN_WAIT_AVAILABLE;
     }
@@ -105,6 +107,12 @@ public class VMSupport {
         );
 
         COMPILERS_AVAILABLE = C1_AVAILABLE || C2_AVAILABLE;
+
+        BIASED_LOCKING_AVAILABLE = detect("Checking for biased locking support",
+                SimpleTestMain.class,
+                null,
+                "-XX:+UseBiasedLocking"
+        );
 
         // Tests are supposed to run in a very tight memory constraints:
         // the test objects are small and reused where possible. The footprint
@@ -260,6 +268,17 @@ public class VMSupport {
         }
     }
 
+    private static Config prependArgs(Config orig, String... args) {
+        return prependArgs(orig, Arrays.asList(args));
+    }
+
+    private static Config prependArgs(Config orig, List<String> args) {
+        List<String> l = new ArrayList<>();
+        l.addAll(args);
+        l.addAll(orig.origArgs());
+        return new Config(l, orig.onlyIfC2(), orig.stress());
+    }
+
     public static void detectAvailableVMConfigs(boolean splitCompilation, List<String> jvmArgs, List<String> jvmArgsPrepend) {
         System.out.println("Probing what VM configurations are available:");
         System.out.println(" (failures are non-fatal, but may miss some interesting cases)");
@@ -291,6 +310,16 @@ public class VMSupport {
             }
         }
 
+        // Mix in locking arguments, if available
+        if (BIASED_LOCKING_AVAILABLE) {
+            LinkedHashSet<Config> newConfigs = new LinkedHashSet<>();
+            for (Config c : configs) {
+                newConfigs.add(prependArgs(c, "-XX:+UseBiasedLocking"));
+                newConfigs.add(prependArgs(c, "-XX:-UseBiasedLocking"));
+            }
+            configs = newConfigs;
+        }
+
         // Mix in input arguments, if available
         List<String> inputArgs = new ArrayList<>();
 
@@ -301,22 +330,16 @@ public class VMSupport {
         }
 
         if (!inputArgs.isEmpty()) {
-            configs = configs.stream().map(c -> {
-                List<String> l = new ArrayList<>();
-                l.addAll(inputArgs);
-                l.addAll(c.origArgs());
-                return new Config(l, c.onlyIfC2(), c.stress());
-            }).collect(Collectors.toCollection(LinkedHashSet::new));
+            configs = configs.stream()
+                    .map(c -> prependArgs(c, inputArgs))
+                    .collect(Collectors.toCollection(LinkedHashSet::new));
         }
 
         // Mix in prepends, if available
         if (jvmArgsPrepend != null) {
-            configs = configs.stream().map(c -> {
-                List<String> l = new ArrayList<>();
-                l.addAll(jvmArgsPrepend);
-                l.addAll(c.origArgs());
-                return new Config(l, c.onlyIfC2(), c.stress());
-            }).collect(Collectors.toCollection(LinkedHashSet::new));
+            configs = configs.stream()
+                    .map(c -> prependArgs(c, jvmArgsPrepend))
+                    .collect(Collectors.toCollection(LinkedHashSet::new));
         }
 
         System.out.println();
