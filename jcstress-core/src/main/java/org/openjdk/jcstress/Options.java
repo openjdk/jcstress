ce8a2c7747e0232fac66783c89ea4a58a3819e0f
Aleksey Shipilëv <shipilev@amazon.de>
2023-06-12 18:16:58 +0200

    7903493: jcstress: Cut down test configurations

--- jcstress-core/src/main/java/org/openjdk/jcstress/Options.java
+++ jcstress-core/src/main/java/org/openjdk/jcstress/Options.java
@@ -218,7 +218,7 @@ public class Options {
             this.pretouchHeap = false;
         } else {
             this.forks = 1;
-            this.forksStressMultiplier = 5;
+            this.forksStressMultiplier = 3;
             this.strideSize = 256;
             this.strideCount = 40;
             this.pretouchHeap = true;
