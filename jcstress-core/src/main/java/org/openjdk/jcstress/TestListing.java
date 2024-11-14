371303783f812f0e3cae2d53dcc9f741e7d01d14
Jiri <jvanek@redhat.com>
2024-11-14 17:37:29 +0100

    Refactored lsiting so it takes proper argument
    instead of misusing verbose switch
    and thus returned the ability to print allavailable tetest

--- jcstress-core/src/main/java/org/openjdk/jcstress/TestListing.java
+++ jcstress-core/src/main/java/org/openjdk/jcstress/TestListing.java
@@ -30,6 +30,18 @@ import java.util.TreeSet;
 
 public class TestListing {
 
+    public enum ListingTypes {
+        NONE, ALL, ALL_MATCHING, ALL_MATCHING_COMBINATIONS;
+
+        public static String toDescription() {
+            return "Optional parameter is: "
+                    + ALL + " - all tests; "
+                    + ALL_MATCHING + " all tests eligible for this system and configuration (like CPU count); "
+                    + ALL_MATCHING_COMBINATIONS + " all real combinations which will run in this setup." +
+                    " Defaults to " + ALL_MATCHING + " if none provided.";
+        }
+    }
+
     private final JCStress jcstress;
 
     public TestListing(JCStress jcstress) {
@@ -39,17 +51,27 @@ public class TestListing {
     public int listTests() {
         JCStress.ConfigsWithScheduler configsWithScheduler = jcstress.getConfigs();
         Set<String> testsToPrint = new TreeSet<>();
-        for (TestConfig test : configsWithScheduler.configs) {
-            if (jcstress.opts.verbosity().printAllTests()) {
-                testsToPrint.add(test.toDetailedTest());
-            } else {
-                testsToPrint.add(test.name);
-            }
-        }
-        if (jcstress.opts.verbosity().printAllTests()) {
-            jcstress.out.println("All matching tests combinations - " + testsToPrint.size());
-        } else {
-            jcstress.out.println("All matching tests - " + testsToPrint.size());
+        switch (jcstress.opts.listingType()) {
+            case ALL_MATCHING_COMBINATIONS:
+                for (TestConfig test : configsWithScheduler.configs) {
+                    testsToPrint.add(test.toDetailedTest());
+                }
+                jcstress.out.println("All matching tests combinations - " + testsToPrint.size());
+                break;
+            case ALL_MATCHING:
+                for (TestConfig test : configsWithScheduler.configs) {
+                    testsToPrint.add(test.name);
+                }
+                jcstress.out.println("All matching tests - " + testsToPrint.size());
+                break;
+            case ALL:
+                for (String test : jcstress.getTests()) {
+                    testsToPrint.add(test);
+                }
+                jcstress.out.println("All existing tests combinations - " + testsToPrint.size());
+                break;
+            default:
+                throw new RuntimeException("Invalid option for listing: " + jcstress.opts.listingType());
         }
         for (String test : testsToPrint) {
             jcstress.out.println(test);
